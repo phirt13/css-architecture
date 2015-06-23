f2f767a14c1c90cac6971fcc0ff8a6238af502f7
Luke Gabrieli <usetheforce10@gmail.com>
2015-06-23 14:22:26 -0700

    add jquery files

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,48 @@
-'use strict';
+// Luke's Exciting jQuery
 
-(function() {
+var url = window.location.href;
+var $links = $("#nav-menu a");
 
-  var currentNav = function() {
-    var navLinks = document.getElementById('nav-menu').getElementsByTagName('a');
-
-    // console.log(navLinks);
-    // used this to understand what I was doing better
-
-    for(var i = 0; i < navLinks.length; i++) {
-      if(document.location.href.indexOf(navLinks[i].href) >= 0) {
-        return navLinks[i].parentNode.className='nav-link-active';
-      }
+$(function(){
+  $($links).each(function(){
+    if ($(this).prop("href") === url){
+      $(this).parent().addClass("nav-link-active");
     }
-  }
+  });
+});
 
-  currentNav();
+// Luke's Exciting jQuery (Alternate)
 
-})();
+// $(function(){
+//   $($links).each(function(){
+//     if (url.indexOf($(this).prop("href")) >= 0){
+//       $(this).parent().addClass("nav-link-active");
+//     }
+//   });
+// });
+
+
+// Philip's Boring Javascript
+
+// 'use strict';
+
+// (function() {
+
+//   var currentNav = function() {
+//     var navLinks = document.getElementById('nav-menu').getElementsByTagName('a');
+
+//     // console.log(navLinks);
+//     // used this to understand what I was doing better
+
+//     for(var i = 0; i < navLinks.length; i++) {
+//       if(document.location.href.indexOf(navLinks[i].href) >= 0) {
+//         return navLinks[i].parentNode.className='nav-link-active';
+//       }
+//     }
+//   }
+
+//   currentNav();
+
+// })();
 
 
